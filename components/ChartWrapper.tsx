c547582885822d8223e434034707ec9973e4dfdd
Danil Hendra <danilhendrasr@gmail.com>
2021-01-19 19:45:57 +0700

    Change ChartWrapper height & item spacing

--- components/ChartWrapper.tsx
+++ components/ChartWrapper.tsx
@@ -1,4 +1,4 @@
 export const ChartWrapper: React.FC<{ bars: JSX.Element[] }> = (props) => {
   const { bars } = props
-  return <div className="flex w-min mx-auto h-screen space-x-2 items-end">{bars}</div>
+  return <div className="flex w-min mx-auto h-5/6 space-x-1 items-end">{bars}</div>
 }
