3fdfe3cb3d4684ec54c43f7aaa5d7b09432b59f7
Danil Hendra <danilhendrasr@gmail.com>
2021-01-22 19:22:08 +0700

    Use Geist UIs color palette for the bars

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -1,8 +1,16 @@
+import { useTheme } from "@geist-ui/react"
+
 export const Bar: React.FC<{ height: number }> = (props) => {
   const { height } = props
+
+  const { palette } = useTheme()
+
   const barStylings: React.CSSProperties = {
     height: `${height}%`,
+    backgroundColor: palette.errorLight,
+    width: "5px",
+    marginRight: 5,
   }
 
-  return <div className="bar bg-red-600 w-1" style={barStylings}></div>
+  return <div className="bar" style={barStylings}></div>
 }
