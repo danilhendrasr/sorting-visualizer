7e0ba2f91c792c25a64658f94b36fa0b5d784944
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2021-03-03 10:36:29 +0700

    Refine home page
    Include:
    - Better layout with sidebar and bars panel side-by-side.
    - Sidebar filled with necessary control components.
    - App title in sidebar.
    - Icon that links to GitHub repo in sidebar.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,16 @@ import { Bar } from "../components/Bar"
 import { ChartWrapper } from "../components/ChartWrapper"
 import { SortButton } from "../components/SortButton"
 import { generateBarHeights, changeBarsColor } from "../utils/index"
-import { Grid, Row, Spacer } from "@geist-ui/react"
+import { Button, ButtonGroup, Slider, Spacer, Text } from "@geist-ui/react"
 import { AlgorithmSelector } from "../components/AlgorithmSelector"
 import { INACTIVE_BAR_COLOR } from "../constants"
 import { SortingAlgorithms, SortingState } from "../types"
 import { animateInsertionSort } from "../algorithms-helper/insertion-sort"
 import { default as Head } from "next/head"
 import { animateBubbleSort } from "../algorithms-helper/bubble-sort"
+import { Github } from "@geist-ui/react-icons"
+// @ts-ignore
+import styles from "../styles/Home.module.css"
 
 const startAnimation = (
   sortingAlgorithm: SortingAlgorithms,
@@ -33,6 +36,7 @@ const startAnimation = (
 const Home: React.FC = () => {
   const [barHeights, setBarHeights] = useState([])
   const [sortState, setSortState] = useState<SortingState>("Sort")
+  const [barLength, setBarLength] = useState(70)
   const [selectedAlgorithm, setSelectedAlgorithm] = useState<string | string[]>(
     "Selection"
   )
@@ -47,16 +51,30 @@ const Home: React.FC = () => {
     />
   ))
 
-  useEffect(() => {
-    const newBarHeights = generateBarHeights(100)
+  const resetBars = (): void => {
+    const newBarHeights = generateBarHeights(barLength)
+    barsEl.current.length = newBarHeights.length
     setBarHeights(newBarHeights)
     setSortState("Sort")
+  }
 
+  const resetBarColors = (): void => {
     // requestAnimationFrame is used to defer the function execution after
     // the browser has finished painting.
     window.requestAnimationFrame(() => {
       changeBarsColor(barsEl.current, INACTIVE_BAR_COLOR)
     })
+  }
+
+  useEffect(() => {
+    resetBars()
+    resetBarColors()
+    console.log(barsEl.current)
+  }, [barLength])
+
+  useEffect(() => {
+    resetBars()
+    resetBarColors()
   }, [selectedAlgorithm])
 
   return (
@@ -65,28 +83,91 @@ const Home: React.FC = () => {
         <title>Sorting Algorithms Visualizer</title>
       </Head>
 
-      <Spacer y={1.3} />
-      <Row justify="center">
-        <ChartWrapper bars={bars} />
-      </Row>
-      <Spacer y={2} />
-      <Row justify="center">
-        <SortButton
-          sortState={sortState}
-          clickAction={() => {
-            setSortState("Sorting")
-            startAnimation(selectedAlgorithm as SortingAlgorithms, barHeights, () =>
-              setSortState("Sorted")
-            )
-          }}
-        />
-        <Spacer x={1} />
-        <AlgorithmSelector
-          disabled={sortState === "Sorting" ? true : false}
-          selectedAlgorithm={selectedAlgorithm}
-          onChangeHandler={setSelectedAlgorithm}
-        />
-      </Row>
+      <div style={{ display: "flex" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            width: 366,
+          }}>
+          <Spacer y={1} />
+          <Text h1 size="2em" style={{ textAlign: "center" }}>
+            Sorting Algorithms Visualizer
+          </Text>
+          <Spacer y={2} />
+          <div>
+            <Text style={{ marginBottom: "5px" }}>Select Algorithm</Text>
+            <AlgorithmSelector
+              disabled={sortState === "Sorting" ? true : false}
+              selectedAlgorithm={selectedAlgorithm}
+              onChangeHandler={setSelectedAlgorithm}
+            />
+          </div>
+          <div style={{ width: "200px" }}>
+            <Text style={{ marginBottom: "10px" }}>Length of array</Text>
+            <Slider
+              disabled={sortState === "Sorting"}
+              min={10}
+              max={100}
+              step={10}
+              onChange={(value) => setBarLength(value)}
+              style={{ width: "100%" }}
+              value={barLength}
+            />
+          </div>
+          <Spacer y={0.6} />
+          <div style={{ width: "200px" }}>
+            <Text style={{ marginBottom: "3px" }}>Speed</Text>
+            <ButtonGroup
+              disabled={sortState === "Sorting"}
+              ghost
+              size="small"
+              style={{ margin: "5px" }}
+              type="secondary">
+              <Button>Slow</Button>
+              <Button>Normal</Button>
+              <Button>Fast</Button>
+            </ButtonGroup>
+          </div>
+          <Spacer y={1.5} />
+          <SortButton
+            sortState={sortState}
+            clickAction={() => {
+              setSortState("Sorting")
+              startAnimation(selectedAlgorithm as SortingAlgorithms, barHeights, () =>
+                setSortState("Sorted")
+              )
+            }}
+          />
+          <Spacer y={0.6} />
+          <Button
+            disabled={sortState === "Sorting"}
+            ghost
+            onClick={() => {
+              resetBars()
+              resetBarColors()
+            }}
+            type="error">
+            Reset
+          </Button>
+          <Spacer y={2.5} />
+          <a
+            className={styles["github-icon-wrapper"]}
+            href="https://github.com/danilhendrasr/sorting-visualizer"
+            target="_blank">
+            <Github size={20} color="#000" />
+          </a>
+        </div>
+        <div
+          style={{
+            justifyContent: "center",
+            width: "calc(100vw - 366px)",
+            alignItems: "center",
+          }}>
+          <ChartWrapper bars={bars} />
+        </div>
+      </div>
     </>
   )
 }
