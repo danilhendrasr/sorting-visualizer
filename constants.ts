a7df17aafca3944c8a98423b0b9ca5a47cd9deee
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2021-03-08 15:36:13 +0700

    Remove SORTING_SPEED from constants

--- constants.ts
+++ constants.ts
@@ -2,5 +2,4 @@ import { SortingAlgorithms } from "./types"
 
 export const ACTIVE_BAR_COLOR = "#111"
 export const INACTIVE_BAR_COLOR = "#ff1a1a"
-export const SORTING_SPEED = 50
 export const ALGORITHMS_LIST: SortingAlgorithms[] = ["Selection", "Insertion", "Bubble"]
