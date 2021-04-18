13d51aee44226aeea24a0244dc3fd8a888e73461
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2021-04-18 22:24:12 +0700

    Add jsDoc to useControlsDisabled hook

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,6 +1,10 @@
 import { useContext } from "react"
 import { AppStateContext } from "../contexts/app-state"
 
+/**
+ * See if any form of sorting controls should be disabled
+ * @returns Are controls should be disabled
+ */
 const useControlsDisabled = () => {
   const sortingState = useContext(AppStateContext).sortingState
   const isControlsDisabled = sortingState === "Sorting"
