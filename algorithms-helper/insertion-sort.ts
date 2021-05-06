6d9e0a49e468cf07031b07ffb0ad2279afb585cc
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2021-05-06 17:02:16 +0700

    New insertion sort code
    With animation steps adhereing to the new animation step format

--- algorithms-helper/insertion-sort.ts
+++ algorithms-helper/insertion-sort.ts
@@ -1,99 +1,103 @@
-// TODO: Refactor all helper functions in this file
 import { AnimateFunctionParams } from "../types"
-import { getAllBars, changeBarsColor, makeBarsActive, postSortAnimation } from "../utils"
+import {
+  getAllBars,
+  changeBarsColor,
+  postSortAnimation,
+  getNumberValueFromElementHeight,
+} from "../utils"
 
-interface InsertionAnimationSequence {
-  idx: number
-  moveFrom: number
-  shift: number
+type TwoNumber = [number, number]
+
+interface AnimationStep {
+  compare?: TwoNumber
+  correctOrder?: TwoNumber
+  swap?: TwoNumber
+  wrongOrder?: TwoNumber
 }
 
-const insertionSort = (arrayToSort: number[]): InsertionAnimationSequence[] => {
-  // TODO: Add more key to the animation steps. E.g: correctOrder, wrongOrder, etc.
-  let animaSeq: InsertionAnimationSequence[] = []
-  let sortedArray: number[] = []
+const getInsertionSortAnimationSteps = (inputArray: number[]) => {
+  let animationSteps: AnimationStep[] = []
+  const array = [...inputArray]
 
-  for (let i = 0; i < arrayToSort.length; i++) {
-    const currentItem = arrayToSort[i]
+  for (let i = 1; i < array.length; i++) {
+    const currentItem = array[i]
 
-    const isFirstIteration = i === 0
-    if (isFirstIteration) {
-      animaSeq.push({ idx: 0, moveFrom: i, shift: 0 })
-      sortedArray.push(currentItem)
-      continue
-    }
+    let greaterLeftSideExists = false
+    for (let j = i - 1; j >= 0; j--) {
+      animationSteps.push({ compare: [i, j] })
+      const leftSide = array[j]
 
-    for (let j = sortedArray.length - 1; j >= 0; j--) {
-      const isLastInneriteration = j === 0
+      if (currentItem < leftSide) {
+        if (j === 0) {
+          animationSteps.push({ correctOrder: [i, j] })
+          animationSteps.push({ swap: [i, j] })
+          const tempArray = array.slice(j, i)
+          array[j] = currentItem
+          array.splice(j + 1, tempArray.length, ...tempArray)
+          break
+        }
 
-      const leftSide = isLastInneriteration ? -Infinity : sortedArray[j - 1]
-      const rightSide = sortedArray[j]
+        animationSteps.push({ wrongOrder: [i, j] })
+        greaterLeftSideExists = true
+      } else {
+        if (greaterLeftSideExists) {
+          animationSteps.push({ correctOrder: [i, j + 1] })
+          animationSteps.push({ swap: [i, j + 1] })
+          const tempArray = array.slice(j + 1, i)
+          array[j + 1] = currentItem
+          array.splice(j + 2, tempArray.length, ...tempArray)
+          break
+        }
 
-      const isLeftSideLesser = leftSide <= currentItem
-      const isRightSideGreater = currentItem <= rightSide
-
-      if (isLeftSideLesser && isRightSideGreater) {
-        const aboveRightSide = sortedArray.splice(j)
-
-        animaSeq.push({
-          idx: sortedArray.length,
-          moveFrom: i,
-          shift: aboveRightSide.length,
-        })
-
-        sortedArray.push(currentItem)
-        sortedArray = sortedArray.concat(aboveRightSide)
+        animationSteps.push({ correctOrder: [i, j] })
         break
-      } else if (isLastInneriteration) {
-        animaSeq.push({ idx: sortedArray.length, moveFrom: i, shift: 0 })
-        sortedArray.push(currentItem)
       }
     }
   }
 
-  return animaSeq
+  return animationSteps
 }
 
 export const animateInsertionSort = (params: AnimateFunctionParams) => {
-  const { barHeights, palette, sortingSpeed, callback } = params
-  const animationSequence = insertionSort(barHeights)
-  const bars = getAllBars()
+  const { palette, sortingSpeed, callback } = params
+  const bars = Array.from(getAllBars())
+  const barHeights = bars.map((bar) =>
+    getNumberValueFromElementHeight(bar.style.height)
+  )
+  const animationSteps = getInsertionSortAnimationSteps(barHeights)
 
-  for (let i = 0; i < animationSequence.length; i++) {
-    const {
-      idx: idxToInsertTo,
-      moveFrom: barToMove,
-      shift: rightShift,
-    } = animationSequence[i]
-    const isFirstIteration = i === 0
-    const isLastIteration = i === animationSequence.length - 1
-
-    let prevIdxBarHeights = []
-
+  let prevActiveBars: HTMLElement[]
+  animationSteps.forEach((step, idx) => {
     setTimeout(() => {
-      if (!isFirstIteration) {
-        changeBarsColor(bars[animationSequence[i - 1].idx], palette.idle)
+      if (prevActiveBars) changeBarsColor(prevActiveBars, palette.idle)
+
+      if (step.compare) {
+        prevActiveBars = step.compare.map((idx) => bars[idx])
+        changeBarsColor(prevActiveBars, palette.compare)
+      } else if (step.correctOrder) {
+        prevActiveBars = step.correctOrder.map((idx) => bars[idx])
+        changeBarsColor(prevActiveBars, palette.correctOrder)
+      } else if (step.wrongOrder) {
+        prevActiveBars = step.wrongOrder.map((idx) => bars[idx])
+        changeBarsColor(prevActiveBars, palette.wrongOrder)
+      } else if (step.swap) {
+        const [idx1, idx2] = step.swap
+        prevActiveBars = bars.slice(idx2, idx1 + 1)
+
+        bars[idx2].style.backgroundColor = palette.swap
+        bars[idx2].style.height = bars[idx1].style.height
+
+        for (let i = idx1; i > idx2; i--) {
+          bars[i].style.backgroundColor = palette.swap
+          bars[i].style.height = bars[i - 1].style.height
+        }
       }
 
-      prevIdxBarHeights.push(bars[idxToInsertTo].style.height)
-      makeBarsActive(
-        [{ element: bars[idxToInsertTo], height: barHeights[barToMove] }],
-        palette.compare
-      )
-    }, i * sortingSpeed)
-
-    for (let x = 1; x <= rightShift; x++) {
-      setTimeout(() => {
-        prevIdxBarHeights.push(bars[idxToInsertTo + x].style.height)
-        bars[idxToInsertTo + x].style.height = prevIdxBarHeights[x - 1]
-
-        const isLastInnerIteration = x === rightShift
-        if (isLastInnerIteration && isLastIteration) {
-          changeBarsColor(bars[idxToInsertTo], palette.idle)
-          postSortAnimation(bars, palette.correctOrder)
-          if (callback) callback()
-        }
-      }, i * sortingSpeed)
-    }
-  }
+      if (idx === animationSteps.length - 1) {
+        changeBarsColor(prevActiveBars, palette.idle)
+        callback()
+        postSortAnimation(bars, palette.correctOrder)
+      }
+    }, idx * sortingSpeed)
+  })
 }
